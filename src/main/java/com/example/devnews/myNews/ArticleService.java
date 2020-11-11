10d12a35c84d0797d12e95cf91167da63536ed6b
Bhagyashri0312 <bhagyashri.laddha12@gmail.com>
2020-11-11 05:35:17 +0100

    Created Rest API for the Articles

--- src/main/java/com/example/devnews/myNews/ArticleService.java
+++ src/main/java/com/example/devnews/myNews/ArticleService.java
@@ -2,18 +2,22 @@ package com.example.devnews.myNews;
 
 
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RequestBody;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Service
 public class ArticleService {
+    private static Long idCounter = 1L;
     private static List<Article> articleList = new ArrayList<>();
 
     static{
-        articleList.add(new Article( 1L, "10 reasons to learn Spring", "In this article I'll be listing 10 reasons why you should learn spring and use it in your next project...", "John Smith" ));
-        articleList.add(new Article( 2L, "Winters in Stockholm", "In this article I'll be telling about the wintes in Stockholm..", "Chris Bay" ));
+        add(new Article( null, "10 reasons to learn Spring", "In this article I'll be listing 10 reasons why you should learn spring and use it in your next project...", "John Smith" ));
+        add(new Article( null, "Winters in Stockholm", "In this article I'll be telling about the winters in Stockholm..", "Chris Bay" ));
     }
 
     //Get a list of all the articles
@@ -31,4 +35,22 @@ public class ArticleService {
                 .findFirst();
     }
 
+
+    public Article create(Article newArticle) {
+        add(newArticle);
+        return newArticle;
+    }
+
+    static private boolean add(Article newArticle){
+        //generate and set the id
+        newArticle.setId(idCounter);
+        idCounter++;
+        return articleList.add(newArticle);
+    }
+
+    public void delete(Long id){
+        articleList= articleList.stream()
+                .filter(t -> !t.getId().equals(id))
+                .collect(Collectors.toList());
+    }
 }
